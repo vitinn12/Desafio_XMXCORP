5fade8fe47c814bcd89b672fa02a0f2937c4c189
vitinn12 <vitoralves1212.va@gmail.com>
2026-05-20 12:54:26 -0300

    projeto finalizado. finalizei a responsividade, fiz correcoes no codigo e no css, e adicionei uma biblioteca no js para fazer animacoes de componentes do html

--- scr/main.js
+++ scr/main.js
@@ -1,7 +1,51 @@
-window.revelar = ScrollReveal ({reset: true})
+const revelar = ScrollReveal({ reset: true });
 
-revelar.reveal('imagem_suplemento_cabecalho',{
-    duration:1000,
+revelar.reveal('.imagem_suplemento_cabecalho', {
+    duration: 2000,
     distance: '200px',
-    reset: false
+    origin: 'right',
+    reset: false,
+});
+revelar.reveal('.imagem_comprimido1_cabecalho, .imagem_folha1_cabecalho, .imagem_folha2_cabecalho, .imagem_comprimido2_cabecalho  ', {
+    duration: 4400,
+    distance: '200px',
+    origin: 'right',
+    reset: false,
+});
+
+revelar.reveal('.suplemento_sessao_informacoes', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'left',
+    reset: false,
+});
+revelar.reveal('.texto_info_suplemento', {
+    duration: 5500,
+    distance: '200px',
+    origin: 'right',
+    reset: false,
+});
+revelar.reveal('.texto_cabecalho_avaliacao_pessoas', {
+    duration: 2000,
+    distance: '200px',
+    origin: 'top',
+    reset: false,
+});
+revelar.reveal('.comp_avaliacao', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'right',
+    reset: false,
+});
+revelar.reveal('.imagem_sessao_garantia_cartao', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'left',
+    reset: false,
+});
+revelar.reveal('.texto_garantia_cartao', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'right',
+    reset: false,
 });
